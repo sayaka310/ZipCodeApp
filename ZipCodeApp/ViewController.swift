fd4ec1491ad7f1e8ac9f8f82019d11fb373e1fdf
sayaka310 <syk3887@yahoo.co.jp>
2021-11-07 11:00:27 +0900

    アプリ完成

--- ZipCodeApp/ViewController.swift
+++ ZipCodeApp/ViewController.swift
@@ -12,7 +12,6 @@ struct ZipCloudResponse: Codable {
     let results : [Address]?
     let status : Int
 }
-
 struct Address: Codable {
     let address1: String
     let address2: String
@@ -29,7 +28,6 @@ struct Address: Codable {
     func kana() -> String {
         return kana1 + kana2 + kana3
     }
-    
 }
 
 class ViewController: UIViewController {
@@ -40,13 +38,11 @@ class ViewController: UIViewController {
     var results: [Address] = []
     let baseUrlStr = "https://zipcloud.ibsnet.co.jp/api/search?zipcode="
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         zipCodeSearchBar.delegate = self
         addressTableView.dataSource = self
     }
-    
     func requestAddressFromZipCode(zipCode: String) {
         
         var responseData: ZipCloudResponse?       // データ保存用変数
@@ -64,49 +60,54 @@ class ViewController: UIViewController {
                 // do{ }でtryが失敗した場合の処理
                 print(error.localizedDescription)
             }
-            // 住所情報が合ったら保存
-            if responseData?.results != nil {
-                self.results = (responseData?.results)!  // 取得したdataのresultsを
-            }
-            
-            // TableViewを再表示
             DispatchQueue.main.async {
+                if responseData?.results != nil {
+                    self.results = (responseData?.results)!
+                } else {
+                    self.showAlert(title: "エラー！", message: "存在しない番号です")
+                }
+                // TableViewを再表示
+                //            DispatchQueue.main.async {
                 self.addressTableView.reloadData()
             }
         }
         task.resume()
-        
     }
-    
+    func isZipCode(enteredText: String) ->Bool {
+        let pattern = "^[0-9]{7}$"
+        guard let regex = try? NSRegularExpression(pattern: pattern) else { return false }
+        let matches = regex.matches(in: enteredText, range: NSRange(location:0, length:enteredText.count))
+        return matches.count == 1 ? true : false
+    }
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert) // 表示作成
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)           // ボタン作成
+        alert.addAction(alertAction)                                                          // 表示にボタンを追加
+        present(alert, animated: true)                                                        // 画面に表示
+    }
 }
 extension ViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         guard let searchText = searchBar.text
         else { return }
+        guard isZipCode(enteredText: searchText)
+        else {
+            showAlert(title: "エラー!", message: "数字を7文字入力してください")
+            return
+        }
         requestAddressFromZipCode(zipCode: searchText)
+        searchBar.resignFirstResponder()
     }
 }
-
 extension ViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
         return results.count
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "AddressCell", for: indexPath)
         // ここでcellの内容を入れる
         let address = results[indexPath.row]
-        //                       cell.textLabel?.text =
-        //                       address.address1
-        //                       + address.address2
-        //                       + address.address3
-        //                       + "("
-        //                       + address.kana1
-        //                       + address.kana2
-        //                       + address.kana3
-        //                       + ")"
         cell.textLabel?.text = "\(address.address())(\(address.kana()))"
         return cell
     }
